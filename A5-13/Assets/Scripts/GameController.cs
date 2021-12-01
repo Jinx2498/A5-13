1ceba3281d171f47adecbf3eb8130a280b7f6812
Cassidy Barr <90427280+Jinx2498@users.noreply.github.com>
2021-12-01 14:30:59 -0500

    ze maze

--- A5-13/Assets/Scripts/GameController.cs
+++ A5-13/Assets/Scripts/GameController.cs
@@ -1,18 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(MazeConstructor))]
 
 public class GameController : MonoBehaviour {
+    
+    [SerializeField] private PlayerMovement player;
+    // [SerializeField] private Text scoreLabel;
 
     private MazeConstructor generator;
 
+    // private int score;
+    private bool goalReached;
+
+    
     void Start() {
 
         generator = GetComponent<MazeConstructor>();
-        generator.GenerateNewMaze(13, 15);
+        StartNewGame();
 
     }
 
+    private void StartNewGame() {
+
+        // score = 0;
+        // scoreLabel.text = score.ToString();
+
+        StartNewMaze();
+
+    }
+
+    private void StartNewMaze() {
+
+        generator.GenerateNewMaze(13, 15, OnStartTrigger, OnGoalTrigger);
+
+        float x = generator.startCol * generator.hallWidth;
+        float y = 1;
+        float z = generator.startRow * generator.hallWidth;
+        player.transform.position = new Vector3(x, y, z);
+
+        goalReached = false;
+        player.enabled = true;
+
+    }
+
+    void Update() {
+
+        if (!player.enabled) {
+
+            return;
+
+        }
+    }
+
+    private void OnGoalTrigger(GameObject trigger, GameObject other) {
+        
+        Debug.Log("Goal!");
+        goalReached = true;
+
+        // score += 1;
+        // scoreLabel.text = score.ToString();
+
+        Destroy(trigger);
+
+    }
+
+    private void OnStartTrigger(GameObject trigger, GameObject other) {
+
+        if (goalReached) {
+
+            Debug.Log("Finish!");
+            player.enabled = false;
+
+            Invoke("StartNewMaze", 4);
+
+        }
+    }
 }
