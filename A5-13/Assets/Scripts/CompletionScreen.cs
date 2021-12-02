9da216cf703dcaec3bc7281853a18b74f7a2e881
Cassidy Barr <90427280+Jinx2498@users.noreply.github.com>
2021-12-01 23:10:14 -0500

    stuff

--- A5-13/Assets/Scripts/CompletionScreen.cs
+++ A5-13/Assets/Scripts/CompletionScreen.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CompletionScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
         Scores score = GameObject.Find("Scores").GetComponent<Scores>();
